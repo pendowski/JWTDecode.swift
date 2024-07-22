d4c33b29843334e473c3e9c5478c5399be909cad
Jarek Pendowski <jarek@pendowski.com>
2024-07-22 16:27:32 +0200

    Moving target to iOS 13, since it should still work

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "JWTDecode",
-    platforms: [.iOS(.v14), .macOS(.v11), .tvOS(.v14), .watchOS(.v7), .visionOS(.v1)],
+    platforms: [.iOS(.v13), .macOS(.v11), .tvOS(.v13), .watchOS(.v7), .visionOS(.v1)],
     products: [.library(name: "JWTDecode", targets: ["JWTDecode"])],
     dependencies: [
     ],
